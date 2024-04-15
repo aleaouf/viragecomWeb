087ade7aa9f5cebdd5eb298e4bd6ff1209586527
sadokaouadi <sadok.aouadi@esprit.tn>
2024-04-15 12:08:02 +0100

    crud + controle de saise

---
+++ src/Controller/PanierController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Panier;
+use App\Entity\Articles;
+use App\Form\PanierType;
+use App\Repository\PanierRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticlesRepository;
+
+#[Route('/panier')]
+class PanierController extends AbstractController
+{
+    #[Route('/', name: 'app_panier_index', methods: ['GET'])]
+public function index(PanierRepository $panierRepository, ArticlesRepository $articlesRepository): Response
+{
+    $paniers = $panierRepository->findAll();
+
+    // Initialiser un tableau pour contenir les articles associés
+    $articles = [];
+
+    // Parcourez chaque panier et récupérez l'article associé
+    foreach ($paniers as $panier) {
+        $article = $articlesRepository->find($panier->getIdArticle());
+        $articles[$panier->getIdPanier()] = $article;
+    }
+
+    return $this->render('panier/index.html.twig', [
+        'paniers' => $paniers,
+        'articles' => $articles,
+    ]);
+}
+
+    #[Route('/new/{id}', name: 'app_panier_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, int $id): Response
+    {
+        $panier = new Panier();
+        
+        // Assurez-vous que l'article existe
+        $article = $entityManager->getRepository(Articles::class)->find($id);
+        if (!$article) {
+            throw $this->createNotFoundException('Article non trouvé.');
+        }
+    
+        // Associez l'ID de l'article au panier
+        $panier->setIdArticle($article->getId());
+        $panier->setIdUser(7);
+    
+        // Créez le formulaire et traitez la demande
+        $form = $this->createForm(PanierType::class, $panier);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($panier);
+            $entityManager->flush();
+    
+            return $this->redirectToRoute('app_panier_index', [], Response::HTTP_SEE_OTHER);
+        }
+    
+        return $this->render('panier/new.html.twig', [
+            'panier' => $panier,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{idPanier}', name: 'app_panier_show', methods: ['GET'])]
+    public function show(Panier $panier): Response
+    {
+        return $this->render('panier/show.html.twig', [
+            'panier' => $panier,
+        ]);
+    }
+
+    #[Route('/{idPanier}/edit', name: 'app_panier_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Panier $panier, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PanierType::class, $panier);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_panier_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('panier/edit.html.twig', [
+            'panier' => $panier,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{idPanier}', name: 'app_panier_delete', methods: ['POST'])]
+    public function delete(Request $request, Panier $panier, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$panier->getIdPanier(), $request->request->get('_token'))) {
+            $entityManager->remove($panier);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_panier_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Articles.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Articles
+ *
+ * @ORM\Table(name="articles")
+  * @ORM\Entity(repositoryClass=App\Repository\ArticlesRepository::class)
+
+ */
+class Articles
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255, nullable=false)
+     */
+    #[Assert\NotBlank(message:"nom doit etre non vide")]
+    #[Assert\Length(min:7,max:10, minMessage:"Doit etre > 7.", maxMessage:"Doit etre <=10")]
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255, nullable=false)
+     */
+    #[Assert\NotBlank(message:"description doit etre non vide")]
+    #[Assert\Length(min:10, minMessage:"Doit etre > 10")]
+    private $description;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="prix", type="float", precision=10, scale=0, nullable=false)
+     */
+    #[Assert\NotBlank(message:"prix doit etre non vide")]
+    #[Assert\Regex(pattern: "/^\d+$/", message: "Le prix ne doit contenir que des chiffres." )]
+    private $prix;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="image", type="string", length=255, nullable=true)
+     */
+    
+    private $image;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="contact", type="integer", nullable=false)
+     */
+    #[Assert\NotBlank(message:"contact doit etre non vide")]
+    #[Assert\Length(min:7,max:10, minMessage:"Doit etre > 7.", maxMessage:"Doit etre <=100")]
+    private $contact;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantite", type="integer", nullable=false)
+     */
+    #[Assert\NotBlank(message:"quantite doit etre non vide")]
+    #[Assert\Type(
+        type: 'integer',
+        message: "Le quantite doit être un nombre."
+    )]
+    private $quantite;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getContact(): ?int
+    {
+        return $this->contact;
+    }
+
+    public function setContact(int $contact): static
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): static
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+
+}
